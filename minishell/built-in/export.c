28326e9977e7629f4ffc8b51282fb0eed181511d
expoilderk <falecommarlonreis@gmail.com>
2023-01-17 23:54:49 -0300

    built-in export

--- minishell/built-in/export.c
+++ minishell/built-in/export.c
@@ -6,7 +6,7 @@
 /*   By: mreis-me <mreis-me@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 11:50:35 by mreis-me          #+#    #+#             */
-/*   Updated: 2023/01/17 21:50:08 by mreis-me         ###   ########.fr       */
+/*   Updated: 2023/01/17 23:52:46 by mreis-me         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,20 @@ int is_valid(char *str)
 	int i;
 
 	i = 0;
+	if (!ft_isalpha(str[0]))
+			return (0);
 	while (str[i])
 	{
-		if (!ft_isalpha(str[i]) && str[i] != '=')
+		if (!ft_isalnum(str[i]))
+		{
+			if (str[i] == '=')
+				break ;
 			return (0);
+		}
 		i++;
 	}
+	if (str[i] != '=')
+		return -1;
 	return (1);
 }
 
@@ -56,37 +64,49 @@ int	export(t_cmd *cmd)
 {
 	char	**export;
 	int		len;
+	int		i;
 
-	if (!is_valid(cmd->args[1]))
+	i = 1;
+	while (cmd->args[i])
 	{
-		printf("export: '%s': não é um identificador válido\n", cmd->args[1]);
-		return (EXIT_FAILURE);
-	}
-
-	// Atualiza variavel de ambiente
-	char *str = ft_strfind(cmd->args[1], '=');
-	if(get_env(str, cmd->env))
-	{
-		len = 0;
-		export = envdup(cmd->env);
-		while (export[len])
+		// Faz a validação
+		if (is_valid(cmd->args[i]) == -1)
+			continue;
+		else if (!is_valid(cmd->args[i]))
 		{
-			if (!ft_strncmp(ft_strfind(export[len], '='), str, -1))
-				export[len] = ft_strndup(cmd->args[1], -1);
-			len++;
+			printf("export: '%s': não é um identificador válido\n", cmd->args[1]);
+			continue;
 		}
-		len = envlen(export);
-		memcpy(cmd->env, export, sizeof(char*) * (len + 1)); //Usar minha memcpy
+
+		// Atualiza variavel de ambiente
+		char *str = ft_strfind(cmd->args[i], '=');
+		if(get_env(str, cmd->env))
+		{
+			len = 0;
+			export = envdup(cmd->env);
+			while (export[len])
+			{
+				if (!ft_strncmp(ft_strfind(export[len], '='), str, -1))
+					export[len] = ft_strndup(cmd->args[i], -1);
+				len++;
+			}
+			len = envlen(export);
+			memcpy(cmd->env, export, sizeof(char*) * (len + 1)); //Usar minha memcpy
+		}
+		else // Cria variavel de ambiente
+		{
+			len = envlen(cmd->env);
+			export = envdup(cmd->env);
+			export[len] = ft_strndup(cmd->args[i], -1);
+			export[len+1] = NULL;
+			len = envlen(export);
+			memcpy(cmd->env, export, sizeof(char*) * (len + 1)); //Usar minha memcpy
+		}
+		free(str);
+
+		i++;
 	}
-	else // Cria variavel de ambiente
-	{
-		len = envlen(cmd->env);
-		export = envdup(cmd->env);
-		export[len] = ft_strndup(cmd->args[1], -1);
-		export[len+1] = NULL;
-		len = envlen(export);
-		memcpy(cmd->env, export, sizeof(char*) * (len + 1)); //Usar minha memcpy
-	}
-	free(str);
+
+
 	return (EXIT_SUCCESS);
 }
